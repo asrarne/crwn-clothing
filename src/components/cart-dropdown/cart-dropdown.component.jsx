c179dc8343f10bd9088e618be47d84026023fb1b
Abhijeet <aabhijeet@gmail.com>
2022-02-03 19:57:12 +0530

    added new cart-item component and render it on cart-dropdown component and also updated total no of cart item in cart icon count

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import CustomButton from '../custom-button/custom-button.component';
+import { connect } from 'react-redux';
+import CartItem from '../cart-item/cart-item.component';
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = () => (
+const CartDropdown = ({cartItems}) => (
     <div className='cart-dropdown'>
-        <div className='cart-item'/>
+        <div className='cart-item'>
+            {cartItems.length === 0 ? <span className='text'>Your cart is empty</span> :
+                cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem}/>)}
+        </div>
         <CustomButton type="button">GO TO CHECKOUT</CustomButton>    
     </div>
 )
 
-export default CartDropdown;
+const mapStateToProps = (state) => ({
+    cartItems: state.cart.cartItems
+});
+
+export default connect(mapStateToProps)(CartDropdown);
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,14 +5,18 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import { toggleCartHidden } from '../../redux/cart/cart-actions';
 import './cart-icon.styles.scss';
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({cartItemCount, toggleCartHidden}) => (
     <div className='cart-icon' onClick={toggleCartHidden} >
         <ShoppingIcon className='shopping-icon' />   
-        <span className='item-count'> 0 </span> 
+        <span className='item-count'>{cartItemCount}</span> 
     </div>
 );
+
+const mapStateToProps = (state) => ({
+    cartItemCount: state.cart.cartItems.length
+})
 
 const mapDispatchToProps = (dispatch) => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
